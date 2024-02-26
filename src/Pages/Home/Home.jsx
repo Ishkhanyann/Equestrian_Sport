c4873cf302d6806d0af1e8816b660f4a174fd964
Ishkhanyann <meriko.yan13@gmail.com>
2024-02-26 19:53:05 +0400

    img adding

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import "./Home.scss"
+import React from "react";
+import "./Home.scss";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div>
+        <h1>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo,
+          mollitia?
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          itaque nam vero ullam beatae aliquid illo, facilis omnis in sed minima
+          corporis magni est ipsa, repellat veritatis pariatur at.
+        </p>
+      </div>
+      <img src="../../public/H/H-main.jpg" alt="" />
     </div>
-  )
+  );
 }
